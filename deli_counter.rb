e3aaa88909b66331010dddec665ef4a7b4b899c6
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-25 15:58:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else katz_deli.any?
     katz_deli = katz_deli.map.with_index {|n, i| (i + 1).to_s + ". " + n}
-    puts "The line is currently #{katz_deli.join(" ")}"
+    puts "The line is currently: #{katz_deli.join(" ")}"
   end
 end
 
