081b6e451de70a57a86f1d86e8605fc3395b5273
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-25 15:27:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else katz_deli.any?
+    puts "The line is currently #{katz_deli.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli,person)
